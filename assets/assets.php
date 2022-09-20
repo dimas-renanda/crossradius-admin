4369d7b5695ed9368593a133d6809457e5ba6dce
dimas-renanda <100029762+dimas-renanda@users.noreply.github.com>
2022-09-20 16:47:33 +0700

    Update assets.php

--- assets/assets.php
+++ assets/assets.php
@@ -10,4 +10,6 @@
 echo '<link href="http://'.$_SERVER['HTTP_HOST'].'/xradius/crossradius-admin/assets/css/bootstrap.css" rel="stylesheet">';
 echo '<script src="http://'.$_SERVER['HTTP_HOST'].'/xradius/crossradius-admin/assets/js/bootstrap.bundle.min.js"></script>';
 ?>
+
+<link rel="icon" href="http://phoenix.crossnet.co.id/xradius/crossradius-admin/assets/img/crosstab.png">
 </html>
