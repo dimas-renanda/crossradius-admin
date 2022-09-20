0aacb11455704bbf0a43eb560199ebd3942615aa
dimas-renanda <100029762+dimas-renanda@users.noreply.github.com>
2022-09-20 16:37:57 +0700

    fix domain

--- assets/assets.php
+++ assets/assets.php
@@ -7,7 +7,7 @@
 
 
 <?php 
-echo '<link href="http://'.$_SERVER['HTTP_HOST'].'/crossradius/assets/css/bootstrap.css" rel="stylesheet">';
-echo '<script src="http://'.$_SERVER['HTTP_HOST'].'/crossradius/assets/js/bootstrap.bundle.min.js"></script>';
+echo '<link href="http://'.$_SERVER['HTTP_HOST'].'/xradius/crossradius-admin/assets/css/bootstrap.css" rel="stylesheet">';
+echo '<script src="http://'.$_SERVER['HTTP_HOST'].'/xradius/crossradius-admin/assets/js/bootstrap.bundle.min.js"></script>';
 ?>
 </html>
