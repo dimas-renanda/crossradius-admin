f68e23abd16606970e4f12feee011c7d1fec962e
dimas-renanda <100029762+dimas-renanda@users.noreply.github.com>
2022-09-20 22:34:59 +0700

    Update deleterouter.php

--- dashboard/deleterouter.php
+++ dashboard/deleterouter.php
@@ -1,11 +1,10 @@
 <?php
-    header('Content-type: application/json');
+    //header('Content-type: application/json');
     require_once "../conf/conn.php";
     require_once "../conf/connrouter.php";
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // declare + assign
         $rid = $_POST['rid'];
-
         // check data login
         // SELECT * FROM user WHERE email = '$username'
         $sql = "DELETE FROM router_nas WHERE id = ? ";
@@ -14,11 +13,11 @@
         $stmt = $linkadmincnm->prepare($sql);
         $stmt->execute([$rid]);
         
-        // if ($stmt)
-        // {
-        //     header('Location: ' . $_SERVER['HTTP_REFERER']);
-        //     exit;
-        // }
+        if ($stmt)
+        {
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+            exit;
+        }
         
        
     }
