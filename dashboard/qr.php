b934abfe6ea12901699fab01b69115f4f9573735
dimas-renanda <dimasrenanda@gmail.com>
2022-10-20 21:00:59 +0700

    Akses Tamu QR

--- dashboard/qr.php
+++ dashboard/qr.php
@@ -1,7 +1,99 @@
 <?php 
-include "phpqrcode/qrlib.php";
 
-$isi = 'http://phoenix.crossnet.co.id'; 
+// include "phpqrcode/qrlib.php";
+
+// $isi = 'http://phoenix.crossnet.co.id'; 
  
-QRcode::png($isi); 
+// QRcode::png($isi); 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> QR Code </title>
+</head>
+<body class="centered-wrapper">
+ 
+    <style type="text/css">
+
+.centered-wrapper {
+    position: relative;
+    text-align: center;
+}
+.centered-wrapper:before {
+    content: "";
+    position: relative;
+    display: inline-block;
+    width: 0; height: 100%;
+    vertical-align: middle;
+}
+.centered-content {
+    display: inline-block;
+    vertical-align: middle;
+}
+        h2,h3{
+            text-align: center;
+        }
+
+        body{ margin:0 auto; }
+ 
+ 
+        .hasil{
+            text-align: center;
+        }
+
+        .column {
+
+  float: left;
+  width: 50%;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+    </style>
+ 
+    <h2>Akses Tamu</h2>
+    <div class="centered-content">
+        <div class="row"><img src="../assets/img/probolinggo.png" height="75" alt="Kominfo Logo">
+        <div class="column"><img src="../assets/img/kominfo.png" height="80"  alt="probolinggoLogo"></div>
+    </div>
+        
+    </div>
+    
+    <h3><a href="#landingpage">KNM Probolinggo</a></h3>
+ 
+
+
+    <div class="hasil">
+        <?php 
+        if(isset($_GET['v'])){
+ 
+     
+            $isi = $_GET['v'];
+ 
+       
+            include "phpqrcode/qrlib.php"; 
+ 
+
+            $penyimpanan = "temp/";
+ 
+
+            if (!file_exists($penyimpanan))
+             mkdir($penyimpanan);
+
+            QRcode::png($isi, $penyimpanan.'hasil_qrcode.png', QR_ECLEVEL_Q, 10, 5); 
+         
+
+            echo '<img src="'.$penyimpanan.'hasil_qrcode.png">';
+        
+        }
+        ?>
+    </div>
+ <i>"Gunakan internet dengan dengan bijak"</i>
+</body>
+</html>
