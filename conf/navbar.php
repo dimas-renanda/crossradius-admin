c109e765300437fdfc9ff49ff430a19172f39393
dimas-renanda <100029762+dimas-renanda@users.noreply.github.com>
2022-11-02 15:58:11 +0700

    icon

--- conf/navbar.php
+++ conf/navbar.php
@@ -1,10 +1,11 @@
 <?php require_once'../conf/conn.php';    ob_start();
+error_reporting(0);
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <a class="navbar-brand" href="">
   &nbsp;
       <?php echo '<img class="navbar-brand" src="http://'.$domainnya.'/xradius/crossradius-admin/assets/img/xnet.png" alt="XNET Logo" height="40">'; ?>
-      MikroX Radius</a>
+      CNM </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -18,20 +19,20 @@
 
           <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-              Admin
+              <i class="fa fa-cogs"></i> Admin
               </a>
               <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
               <!-- <li>
                         <a class="dropdown-item" href="Guest_loc.php">Count Guest Based Location</a>
                       </li> -->
                       <li>
-                      <a class="dropdown-item" href="../news"> News<span class="sr-only"></span></a>
+                      <a class="dropdown-item" href="../news"><i class="fa fa-newspaper-o"></i> News<span class="sr-only"></span></a>
                       </li>
                       <li>
-                        <a class="dropdown-item" href="../CS/">Customer Services</a>
+                        <a class="dropdown-item" href="../CS/"><i class="fa fa-comments-o" aria-hidden="true"></i> Customer Services</a>
                       </li>
                       <li>
-                        <a class="dropdown-item" href="">Ticketing</a>
+                        <a class="dropdown-item" href="../ticket/"><i class="fa fa-user-times" aria-hidden="true"></i> Ticketing</a>
                       </li>
 
               </ul>
@@ -81,41 +82,43 @@
                       </li>
               </ul>
           </li> -->
-
-          <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-              Radius
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-              <!-- <li>
-                        <a class="dropdown-item" href="Guest_loc.php">Count Guest Based Location</a>
-                      </li> -->
-                      <li>
-                      <a class="dropdown-item" href="../dashboard/cobadash.php"> Dashboard<span class="sr-only"></span></a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="system">System</a>
-                      </li>
-                      <!-- <li>
-                        <a class="dropdown-item" href="best_hotel.php">Restart</a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="best_guest.php">Shutdown</a>
-                      </li> -->
-
-              </ul>
+<?php 
+if($_SESSION["isrouter"] == "masuk")
+{
+  echo '          <li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+  <i class="fa fa-podcast" aria-hidden="true"></i> Radius
+  </a>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+  <!-- <li>
+            <a class="dropdown-item" href="Guest_loc.php">Count Guest Based Location</a>
+          </li> -->
+          <li>
+          <a class="dropdown-item" href="../dashboard/cobadash.php"> <i class="fa fa-desktop" aria-hidden="true"></i> Dashboard<span class="sr-only"></span></a>
           </li>
+          <li>
+            <a class="dropdown-item" href="system"><i class="fa fa-wrench" aria-hidden="true"></i> System</a>
+          </li>
+          <!-- <li>
+            <a class="dropdown-item" href="">Restart</a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="">Shutdown</a>
+          </li> -->
+
+  </ul>
+</li>';
+}
+?>
+
 
           <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                Account
+              <i class="fa fa-user-circle"></i> Account
               </a>
               <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                       <li>
-                        <a class="dropdown-item" href="../">Login</a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="../logout/">Logout</a>
+                        <a class="dropdown-item" href="../logout/"><i class="fa fa-sign-out"></i> Logout</a>
                       </li>
               </ul>
           </li>
