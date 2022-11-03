c0d9aea7838789981f11464be59bc9c520a204e5
dimas-renanda <100029762+dimas-renanda@users.noreply.github.com>
2022-11-03 14:47:57 +0700

    Update uploadmedia.php

--- admin/uploadmedia.php
+++ admin/uploadmedia.php
@@ -34,6 +34,7 @@ $extension = substr($newfilename,strlen($newfilename)-4,strlen($newfilename));
   {
     $stmt = $linkadmincnm->prepare($sql_update);
     $stmt->execute();
+    mysqli_error($linkadmincnm);
     // header('Location: ' . $_SERVER['HTTP_REFERER']);
     //         exit;
 
