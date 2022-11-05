11d53e1edd99150ecf20577648586db32f4b4a3f
dimas-renanda <dimasrenanda@gmail.com>
2022-11-05 20:05:14 +0700

    Update login.php

--- chatcs/login.php
+++ chatcs/login.php
@@ -1,4 +1,4 @@
-<!-- 
+
 <?php 
 if(isset($_POST['name']) && !isset($display_case)){
  $name=htmlspecialchars($_POST['name']);
@@ -14,21 +14,21 @@ if(isset($_POST['name']) && !isset($display_case)){
 }elseif(isset($display_case)){
  if(!isset($ermsg)){
 ?>
- <form action="index.php" method="POST">
+ <!-- <form action="index.php" method="POST">
   <input name="name" class="feedback-input" placeholder="Nama kamu" required/>
   <button id="button-blue">Masuk</button>
- </form>
+ </form> -->
 <?php 
  }else{
 ?>
- <form action="index.php" method="POST">
+ <!-- <form action="index.php" method="POST">
   <input name="name" class="feedback-input" placeholder="Nama kamu" required/>
   <button id="button-blue">Masuk</button>
- </form>
+ </form> -->
 <?php 
   echo $ermsg;
  }
  echo '<p align="center">Ini adalah tempat chat room bersama orang-orang yang tidak anda kenal dan orang lain tidak mengetahui siapa anda. ^o^</p>';
 }
 ?>
-<footer>dibuat by <a href="https://subinsb.com/group-chat-in-php-with-ajax-jquery-mysql" target="_BLANK">@SubinSiby</a> dikembangkan by <a href="http://ibacor.com" target="_BLANK">@bachors</a></footer> -->
+<footer>Leap 2022 XNET <a href="petra.ac.id" target="_BLANK">MikroX</a>  test <a href="http://crossnet.co.id" target="_BLANK">XRADIUS</a></footer> 
