bcd4f8aba9593d3234a6df15061772a0632dd244
dimas-renanda <dimasrenanda@gmail.com>
2022-11-23 17:29:56 +0700

    Update koneksi.php

--- Api/News/koneksi.php
+++ Api/News/koneksi.php
@@ -1,11 +1,10 @@
 <?php
-// Membuat variabel, ubah sesuai dengan nama host dan database pada hosting 
+
 $host	= "localhost";
 $user	= "user";
 $pass	= "xyz13juli";
 $db	= "admin_cnm";
 
-//Menggunakan objek mysqli untuk membuat koneksi dan menyimpan nya dalam variabel $mysqli	
 $mysqli = new mysqli($host, $user, $pass, $db);
 
 ?>
